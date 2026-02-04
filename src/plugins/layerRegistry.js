fe3ddd4b10980c417e34f024b288afc9a0b1dd0d
Simon Ravnic <simon@MacBook-Pro-Simon.local>
2026-02-04 18:48:11 +0100

    N1MM-QSO-UDP ingest

--- src/plugins/layerRegistry.js
+++ src/plugins/layerRegistry.js
@@ -8,6 +8,7 @@ import * as AuroraPlugin from './layers/useAurora.js';
 import * as WSPRPlugin from './layers/useWSPR.js';
 import * as GrayLinePlugin from './layers/useGrayLine.js';
 import * as LightningPlugin from './layers/useLightning.js';
+import * as ContestQsosPlugin from './layers/useContestQsos.js';
 
 const layerPlugins = [
   WXRadarPlugin,
@@ -16,6 +17,7 @@ const layerPlugins = [
   WSPRPlugin,
   GrayLinePlugin,
   LightningPlugin,
+  ContestQsosPlugin,
 ];
 
 export function getAllLayers() {
